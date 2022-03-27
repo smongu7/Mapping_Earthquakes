27594245cdc441ee09913a55ec4e5b7662f13ba5
smong <smongu7@gmail.com>
2022-03-27 18:41:10 -0500

    complete module 13.6.2

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -1,27 +1,46 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Access toronto geoJson URL
-let torontoHoods = "https://raw.githubusercontent.com/smongu7/Mapping_Earthquakes/main/torontoNeighborhoods.json"
-
-// Create line style
-let myLine = {
-  color: "#2e41f1",
-  weight: 1
-}
+// Access earthquake geoJson URL
+let mag = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 // grab geojson data
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
-});
-    // style: myLine,
-    // onEachFeature: function(feature, layer) {
-    //   layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
-    //   + feature.properties.dst + "</h3>");
-//     }
-//   }).addTo(map);
-// });
+  
+  // This function returns the style data for each of the earthquakes we plot on
+  // the map. We pass the magnitude of the earthquake into a function
+  // to calculate the radius.
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: "#ffae42",
+      color: "#000000",
+      radius: getRadius(),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+  // This function determines the radius of the earthquake marker based on its magnitude.
+  // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+}
+
+// Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+    // We turn each feature into a circleMarker on the map.   
+    pointToLayer: function(feature, latlng) {
+                console.log(data);
+                return L.circleMarker(latlng);
+              },
+              // We set the style for each circleMarker using our styleInfo function.
+            style: styleInfo
+            }).addTo(map);
+        });
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
