b10c06477eaf39bdf203555a623bacea54cb2038
smong <smongu7@gmail.com>
2022-03-27 05:10:50 -0500

    complete module 13.5.5

--- Mapping_geojson_linestrings/static/js/logic.js
+++ Mapping_geojson_linestrings/static/js/logic.js
@@ -4,18 +4,33 @@ console.log("working");
 // Access airport geoJson URL
 let airportData = "https://raw.githubusercontent.com/smongu7/Mapping_Earthquakes/main/majorAirports.json"
 
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
+
+// Access Toronto data
+let torontoData = "https://raw.githubusercontent.com/smongu7/Mapping_Earthquakes/main/torontoRoutes.json"
 // grab geojson data
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
   console.log(data);
   // create geojson layer w/ retrieved data
-  L.geoJson(data).addTo(map);
+  L.geoJson(data, {
+    style: myStyle,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
+      + feature.properties.dst + "</h3>");
+    }
+  }).addTo(map);
 });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/light-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
@@ -30,20 +45,20 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
+  Light: light,
   Dark: dark
 };
 
 // Create the map object with a center, zoom level and default layer
 // Note: Setview method is on scale of 0-18
 let map = L.map('mapid', {
-  center: [30, 30],
+  center: [43.6487, -79.3854],
   zoom: 2,
-  layers: [streets]
+  layers: [dark]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+//light.addTo(map);
